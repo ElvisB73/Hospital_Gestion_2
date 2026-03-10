87faf0b2a436cfcdec645ef0d82cc8b230c20d78
ElvisB73 <baezelvis73@gmail.com>
2026-03-10 10:45:34 -0400

    Agregar archivos de proyecto.

---
+++ Capa_Datos/Conexion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace Hospital_Gestion_2
+{
+
+    public class CD_Conexion
+    {
+        private readonly string cadenaConexion =
+            "Server=.; Database=Hospital_Turnos_Traige; Integrated Security=True;";
+
+        public SqlConnection AbrirConexion()
+        {
+            SqlConnection conexion = new SqlConnection(cadenaConexion);
+            if (conexion.State == ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public void CerrarConexion(SqlConnection conexion)
+        {
+            if (conexion.State == ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+
+}
